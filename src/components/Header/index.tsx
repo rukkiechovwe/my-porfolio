acf2a4f77265cdbef9d45cc7455b9138284f0225
rukkiechovwe <rukkiechowe@gmail.com>
2023-05-13 07:13:02 +0100

    fix build error

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,6 @@
 import { FC, useState } from "react";
 import { Moon, SunDim } from "phosphor-react";
-import Image from "next/Image";
+import Image from "next/image";
 
 import * as S from "./styles";
 import { useThemeContext } from "../../context/theme_context";
